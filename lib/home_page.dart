efe60769b04e73cf6638c24b7020044c33ab1d72
harshal0001 <190540107113@darshan.ac.in>
2022-06-28 20:16:38 +0530

    Day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome TO Catalog"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
