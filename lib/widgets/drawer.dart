2647c05fb64abc8ed15b8bde27bea81a7d4d9783
harshal0001 <190540107113@darshan.ac.in>
2022-06-30 13:26:39 +0530

    Day 11 Updated

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -8,7 +8,7 @@ class MyDrawer extends StatelessWidget {
         "https://instagram.fraj1-1.fna.fbcdn.net/v/t51.2885-19/278927169_502754784888142_3327900834349100805_n.jpg?stp=dst-jpg_s320x320&_nc_ht=instagram.fraj1-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=1pSEEoy0xk4AX84tV1V&edm=ABfd0MgBAAAA&ccb=7-5&oh=00_AT_DYX1UY0ieb1M7gw0cVwu5uoKK1SayxINcTWLaIquWEw&oe=62C2B26E&_nc_sid=7bff83";
     return Drawer(
       child: Container(
-        color: Colors.purple,
+        color: Colors.deepPurple,
         child: ListView(
           padding: EdgeInsets.zero,
           children: [
